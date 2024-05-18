e18262d5b736afbf0826335d99278c1481bf40d8
Burhan <m.burhanmoin1@gmail.com>
2024-05-18 18:50:10 +0500

    'css-fi'

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,6 @@ export const metadata = {
   description: "Hameed's is a leading manufacturer and distributor of high-quality leather finishing chemicals in Pakistan.",
   keywords: "leather finishing chemicals, chemical specialties, Pakistan leather chemical, karachi leather chemical, korangi leather finishing",
   author: "Burhan Moin",
-  viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({ children }) {
@@ -21,9 +20,7 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
-        <meta name="author" content={metadata.author} />
-        <meta name="viewport" content={metadata.viewport} />
-        {/* Add more meta tags as needed */}
+        <meta name="author" content={metadata.author} />        {/* Add more meta tags as needed */}
       </Head>
       <html lang="en">
         <body className={inter.className}>{children}</body>
