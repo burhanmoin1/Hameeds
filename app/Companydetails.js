a325bd6aff97e421cf36b0d4ad71ff2f2df9aecb
Burhan Moin <burhan@thecozm.com>
2024-05-20 19:34:17 +0500

    'fix'

--- app/Companydetails.js
+++ app/Companydetails.js
@@ -1,8 +1,5 @@
 import React from "react";
 import aboutusimg from './hameedsaboutus.jpg';
-import Link from "next/link";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 const Companydetails = () => {
     return (
@@ -11,7 +8,6 @@ const Companydetails = () => {
             <h2 className="companyheading">About Us</h2>
             <img src={aboutusimg.src} className="aboutusimg" />
             <p className="companypara">Hameeds is a leading manufacturer and distributor of high-quality leather finishing chemicals in Pakistan. Established in 2020, our manufacturing and packing facility in the industrial hub of Korangi, Karachi, allows us to deliver reliable products to customers in Karachi, Lahore, Kasur, Sialkot, and across the country.<br></br><br></br>Hameeds has a specialized laboratory for conducting basic trials, ensuring that every batch of chemicals meets rigorous quality and effectiveness standards before reaching customers. Hameeds prioritizes customer satisfaction by providing exceptional products and unparalleled service, aiming to build long-lasting relationships and solidify its reputation as a reliable supplier.</p>
-    
         </div>
         </section>
     );
--- app/ContactUs.js
+++ app/ContactUs.js
@@ -1,6 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faShare } from '@fortawesome/free-solid-svg-icons';
-
 const ContactUs = () => {
     return (
         <section id='contactus'>
